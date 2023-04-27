277bc73892abb571172d3fd4275e41caeb852ea7
ikhlosi <105212465+ikhlosi@users.noreply.github.com>
2023-04-27 08:33:15 +0200

    Hide drawer header

--- src/routes/DrawerNav.js
+++ src/routes/DrawerNav.js
@@ -8,7 +8,12 @@ const Drawer = createDrawerNavigator();
 export default DrawerNav = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="HomeStack">
+      <Drawer.Navigator
+        initialRouteName="HomeStack"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
         <Drawer.Screen name="HomeStack" component={HomeStack} />
         <Drawer.Screen name="AboutStack" component={AboutStack} />
       </Drawer.Navigator>
