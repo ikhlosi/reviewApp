d7e8eaf8b82a003768a1e74c4e4c25f6917d62f6
ikhlosi <105212465+ikhlosi@users.noreply.github.com>
2023-04-27 06:44:01 +0200

    passing route params

--- src/screens/ReviewDetails.js
+++ src/screens/ReviewDetails.js
@@ -2,14 +2,16 @@ import { Button, StatusBar, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { globalStyles } from "../styles/global";
 
-const ReviewDetails = ({ navigation }) => {
+const ReviewDetails = ({ navigation, route }) => {
+  const heroDetails = route.params;
   const pressHandler = () => {
     navigation.pop();
   };
   return (
     <View style={globalStyles.container}>
-      <Text>ReviewDetails</Text>
-      <Button onPress={() => pressHandler()} title="Go Back" />
+      <Text>{heroDetails.title}</Text>
+      <Text>{heroDetails.body}</Text>
+      <Text>{heroDetails.rating}</Text>
     </View>
   );
 };
